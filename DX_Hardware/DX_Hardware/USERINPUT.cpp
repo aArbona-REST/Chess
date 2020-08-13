5a76c8827be1143949d6f4fa2bf5534cc5524830
Antonio Arbona <aarbona@outlook.com>
2020-08-13 12:57:15 -0400

    The FBX dll has been removed. The project builds and runs as expected. The previous commits would not run due to a linker error.

--- DX_Hardware/DX_Hardware/USERINPUT.cpp
+++
@@ -1,8 +0,0 @@
-#include "USERINPUT.h"
-USERINPUT::USERINPUT()
-{
-}
-
-USERINPUT::~USERINPUT()
-{
-}
--- DX_Hardware/FBX/FBX.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include "Containers.h"
-
-__declspec(dllexport) void loadOBJ(char * fileName, vector<vert_pos_skinned> & FileMesh, vector<unsigned int> & VertIndices);
-__declspec(dllexport) void function(char * fileName, char * outFileNameMesh, char * outFileNameBone, char * outFileNameAnimations, anim_clip* & animation, vert_pos_skinned* & FileMesh, unsigned int & VertCount, unsigned int * & VertIndices,unsigned int & IndiceCount);
-__declspec(dllexport) void functionality(char * inFileNameMesh, char * inFileNameBone, char * inFileNameAnimations, vector<joint> & bind_pose);
-
